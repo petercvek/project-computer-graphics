b72edb821cc99bacee0880595332a7e6bd079c18
Peter Cvek <petcvek@gmail.com>
2019-12-28 16:28:58 +0100

    Delujoč deepcopy

--- Main.js
+++ Main.js
@@ -9,6 +9,8 @@ import * as CubeModel from "./cube.js";
 import GLTFLoader from "./GLTFLoader.js";
 import Renderer from "./Renderer.js";
 
+import Trees from "./Trees.js";
+
 const mat4 = glMatrix.mat4;
 
 let pozicijaX = 1; // min 0
@@ -17,8 +19,69 @@ let pozicijaZ = 0;
 let premikam = false;
 let smerFigure = 3.14159265;
 
+let trees = new Trees(); // nazačetku takoj doda 10 vrstic dreves (samo da se bo vidlo kako se sproti generirajo, potem bomo na začetku zgeneriral cca. 20 vrstic dreves)
 function round(value, decimals) {
   return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+}
+
+function samoSkoci() {
+  var time = 0;
+  var interval = setInterval(function() {
+    if (time < 50) {
+      if (time > 24) {
+        pozicijaZ = round(pozicijaZ - 0.02, 3);
+      } else {
+        pozicijaZ = round(pozicijaZ + 0.02, 3);
+      }
+      time++;
+    } else {
+      clearInterval(interval);
+      premikam = false;
+    }
+  }, 2);
+}
+
+function deepCopy(obj) {
+  var rv;
+
+  switch (typeof obj) {
+    case "object":
+      if (obj === null) {
+        // null => null
+        rv = null;
+      } else {
+        switch (toString.call(obj)) {
+          case "[object Array]":
+            // It's an array, create a new array with
+            // deep copies of the entries
+            rv = obj.map(deepCopy);
+            break;
+          case "[object Date]":
+            // Clone the date
+            rv = new Date(obj);
+            break;
+          case "[object RegExp]":
+            // Clone the RegExp
+            rv = new RegExp(obj);
+            break;
+          // ...probably a few others
+          default:
+            // Some other kind of object, deep-copy its
+            // properties into a new object
+            rv = Object.keys(obj).reduce(function(prev, key) {
+              prev[key] = deepCopy(obj[key]);
+              return prev;
+            }, {});
+            break;
+        }
+      }
+      break;
+    default:
+      // It's a primitive, copy via assignment
+      rv = obj;
+      break;
+  }
+  return rv;
 }
 
 function premik(smer, usmerjenost) {
@@ -27,8 +90,11 @@ function premik(smer, usmerjenost) {
     if (time < 50) {
       if (smer === "x") {
         if (usmerjenost === "+") {
+          // Naprej - W
           pozicijaX = round(pozicijaX + 0.01, 3);
+          // nova vrstica vedno ko pritisneš [W]
         } else {
+          // Nazaj - S
           pozicijaX = round(pozicijaX - 0.01, 3);
         }
       } else {
@@ -57,20 +123,37 @@ const keydownHandler = e => {
   } else {
     premikam = true;
     if (e.code === "KeyW") {
-      premik("x", "+");
       smerFigure = 3.14159265;
+      if (trees.map[(1.5 + pozicijaX) * 2][(0.5 + pozicijaY) * 2] !== 1) {
+        premik("x", "+");
+      } else {
+        samoSkoci();
+      }
+      trees.nextLine();
     } else if (e.code === "KeyS") {
-      premik("x", "-");
       smerFigure = 0;
+      if (trees.map[(0.5 + pozicijaX) * 2][(0.5 + pozicijaY) * 2] !== 1) {
+        premik("x", "-");
+      } else {
+        samoSkoci();
+      }
     } else if (e.code === "KeyD") {
-      premik("y", "+");
       smerFigure = -1.57079;
+      if (trees.map[(1 + pozicijaX) * 2][(1 + pozicijaY) * 2] !== 1) {
+        premik("y", "+");
+      } else {
+        samoSkoci();
+      }
     } else if (e.code === "KeyA") {
-      premik("y", "-");
       smerFigure = 1.57079;
+      if (trees.map[(1 + pozicijaX) * 2][pozicijaY * 2] !== 1) {
+        premik("y", "-");
+      } else {
+        samoSkoci();
+      }
     }
   }
-  console.log("X: " + pozicijaX, "Y: " + pozicijaY);
+  // console.log("X: " + pozicijaX, "Y: " + pozicijaY);
 };
 const keyupHandler = e => {
   //   console.log(e);
@@ -100,35 +183,56 @@ class App extends Application {
     this.initGL();
 
     const generirajSvet = async () => {
-      for (let i = 0; i < 30; i++) {
-        let loader2 = new GLTFLoader(this.gl);
-        let gltf = await loader2.load("modeli/grass/grass.gltf");
-        let t = gltf.nodes[2].transform;
+      let loader4 = new GLTFLoader(this.gl);
+      let gltf2 = await loader4.load("modeli/vampire/vampire.gltf");
+      this.hero = gltf2;
+
+      let grassLoader = new GLTFLoader(this.gl);
+      let grassGltf = await grassLoader.load("modeli/grass/grass.gltf");
+      let mesh = grassGltf.scenes[0].nodes[0].mesh;
+
+      // let gltfScena = gltf.scenes[0].nodes[0];
+      // console.log(grassGltf);
+
+      for (let i = 0; i < 20; i++) {
+        const gltf = deepCopy(grassGltf); // Object.assign({ index: i }, grassGltf);
+        let t = gltf.scenes[0].nodes[0].transform;
         mat4.fromTranslation(t, [3, 0, 8 - i]);
         mat4.rotateX(t, t, 1.570796);
-        this.vseSceneVIgri.push(gltf.scenes[0].nodes[1]);
+        gltf.scenes[0].nodes[0].mesh = mesh;
+        // console.log(gltf.scenes[0].nodes[0].transform);
+        this.vseSceneVIgri.push(gltf.scenes[0].nodes[0]);
       }
+      // console.log(this.vseSceneVIgri);
 
-      let trees = [
-        [1, 0, 0, 0, 1, 0, 1, 0, 1],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0, 1, 1],
-        [0, 1, 1, 0, 0, 0, 1, 0, 0]
+      let enviromentObjects = [
+        "tree/tree0.gltf",
+        "tree/tree1.gltf",
+        "tree/tree2.gltf",
+        "tree/tree3.gltf",
+        "rock/rock.gltf",
+        "rock/rock2.gltf"
       ];
+      // let drevoLoader = new GLTFLoader(this.gl);
+      // this.drevoGltf = await drevoLoader.load("modeli/tree/tree0.gltf");
 
-      let differentTrees = ["tree0.gltf", "tree1.gltf", "tree2.gltf", "tree3.gltf"];
-      for (let i = 0; i < trees.length; i++) {
-        for (let j = 0; j < trees[i].length; j++) {
-          if (trees[i][j] == 1) {
-            let loader = new GLTFLoader(this.gl);
-            let gltf = await loader.load("modeli/tree/" + differentTrees[Math.floor(Math.random() * differentTrees.length)]);
-            // let gltf = await loader.load("modeli/tree/tree0.gltf");
-            let t = gltf.nodes[2].transform;
+      for (let i = 0; i < trees.map.length; i++) {
+        for (let j = 0; j < trees.map[i].length; j++) {
+          if (trees.map[i][j] == 1) {
+            // let gltfScena = await Object.assign({}, this.drevoGltf.scenes[0]);
+            // console.log(gltfScena.nodes[0].transform);
+
+            // let loader = new GLTFLoader(this.gl);
+
+            let drevoLoader = new GLTFLoader(this.gl);
+            // let gltf = await drevoLoader.load("modeli/" + enviromentObjects[Math.floor(Math.random() * enviromentObjects.length)]);
+            let gltf = await drevoLoader.load("modeli/tree/tree0.gltf");
+            let t = gltf.scenes[0].nodes[0].transform;
             mat4.fromTranslation(t, [j - 1, 0.5, -i + 2]);
             mat4.rotateX(t, t, 1.570796);
-            this.vseSceneVIgri.push(gltf.scenes[0].nodes[1]);
+            this.vseSceneVIgri.push(gltf.scenes[0].nodes[0]);
+            // console.log(gltfScena.nodes[0].transform);
+            // console.log(this.vseSceneVIgri);
           }
         }
       }
@@ -136,14 +240,35 @@ class App extends Application {
       let loader = new GLTFLoader(this.gl);
       let gltf = await loader.load("modeli/vampire/vampire.gltf");
       this.hero = gltf;
+
       this.vseSceneVIgri.push(gltf.scenes[0].nodes[1]);
       this.nalozeno = true;
     };
 
     generirajSvet();
+    this.izrisanihVrstic = trees.map.length;
   }
 
   update() {
+    const izrisiNoveVrstice = async izrisanihVrstic => {
+      for (let i = izrisanihVrstic; i < trees.map.length; i++) {
+        for (let j = 0; j < trees.map[i].length; j++) {
+          if (trees.map[i][j] == 1) {
+            const gltfScena = Object.assign({}, this.drevoGltf.scenes[0]);
+            // const randomObjekt = Object.assign({}, objketi[Math.floor(Math.random() * objketi.length)]);
+            // let loader = new GLTFLoader(this.gl);
+            // let gltf = await loader.load("modeli/" + enviromentObjects[Math.floor(Math.random() * enviromentObjects.length)]);
+            // let gltf = await loader.load("modeli/tree/tree0.gltf");
+            let t = gltfScena.nodes[0].transform;
+            mat4.fromTranslation(t, [j - 1, 0.5, -i + 2]);
+            mat4.rotateX(t, t, 1.570796);
+            this.vseSceneVIgri.push(gltfScena.nodes[0]);
+            console.log(this.vseSceneVIgri);
+          }
+        }
+      }
+    };
+
     // PREMIKANJE IN ROTIRANJE
     if (this.hero) {
       let s1 = this.hero;
@@ -153,11 +278,15 @@ class App extends Application {
         mat4.rotateX(t2, t2, 1.570796);
         mat4.rotateZ(t2, t2, smerFigure);
       }
-
-      this.hero.nodes[0].parent.translation[0] += 0.1;
-      // console.log(camera);
-
-      // mat4.translate(camera, camera, [0, -0.1, 0]);
+      let camera = this.hero.nodes[0];
+      let t4 = camera.transform;
+      mat4.getRotation(t4, t4);
+      mat4.fromRotationTranslation(t4, t4, [0.665 * pozicijaX + 1.33, 0, -1 * pozicijaX - 1]);
+    }
+    if (this.izrisanihVrstic != trees.map.length) {
+      console.log(trees.map.length);
+      // izrisiNoveVrstice(this.izrisanihVrstic);
+      this.izrisanihVrstic++;
     }
   }
 
@@ -172,7 +301,7 @@ class App extends Application {
     if (this.nastaviKamero == true) {
       // nastavi kamero samo enkrat enkrat
       let t4 = camera.transform;
-      mat4.rotateX(t4, t4, -0.2);
+      // mat4.rotateX(t4, t4, -0.2);
       this.nastaviKamero = false;
     }
 
---
+++ Trees.js
@@ -0,0 +1,56 @@
+export default class Trees {
+  constructor() {
+    this.map = Array.from({ length: 1 }, () => []);
+    this.map[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.map[1] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < 20; i++) this.nextLine();
+  }
+
+  nextLine() {
+    let len = this.map.length;
+    if (this.isTreeLine(this.map[len - 2])) {
+      // če je pred prejšnja napolnjena z drevesi doda eno prazno
+      this.map[len] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    } else {
+      let n_trees = Math.floor(Math.random() * 4) + 2; // število dreves naključno med 2-5
+      let next = Array.from(Array(9), () => 0); // napolni z ničlami
+      for (
+        let i = 0;
+        i < n_trees;
+        i++ // začetek tabele napolni z n_trees enkami
+      )
+        next[i] = 1;
+      while (true) {
+        // shufla dokler ni vsaj ene proste poti med trenutno in prejšnjo
+        next = this.shuffle(next);
+        for (let i = 0; i < next.length; i++) {
+          if (this.map[len - 1][i] === 0 && next[i] === 0) {
+            // če sta na istem indeksu obe 0 obstaja pot
+            this.map[len] = next;
+            return;
+          }
+        }
+      }
+    }
+  }
+
+  shuffle(array) {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // swap:
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  isTreeLine(array) {
+    for (let i = 0; i < array.length; i++) if (array[i] === 1) return true;
+    return false;
+  }
+}
